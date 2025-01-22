347ac4cef0ef434413fe9e938523398401bf02f3
l-martinetti <luca.martinetti@outlook.it>
2025-01-23 00:43:44 +0100

    feat: excercise complete

--- src/assets/components/FormBlog.jsx
+++ src/assets/components/FormBlog.jsx
@@ -1,58 +1,197 @@
-const blogTitles = [];
-
-
 import { useState } from 'react'
+
+const categories = [
+    'romanzo',
+    'saggio breve',
+    'trattato'
+]
+
+const tags = [
+    'fantasy',
+    'horror',
+    'thriller'
+]
 
 const FormBlog = () => {
 
-    const [titles, setTitles] = useState(blogTitles);
-    const [newTitle, setNewTitle] = useState('')
+    const initialFormData = {
+        title: '',
+        imgUrl: '',
+        content: '',
+        category: '',
+        tags: [],
+        published: false
+    }
 
-    const handlerSubmit = (e) => {
-        e.preventDefault();
+    const [formData, setFormData] = useState(initialFormData);
+    const [posts, setPosts] = useState([]);
 
-        const newBlogTitles = [newTitle, ...titles];
-        setTitles(newBlogTitles)
+    const handleInputs = (e) => {
+        let value = e.target.value
 
-        setNewTitle('')
-    };
+        if (e.target.name === 'category') value = categories[index]
 
-    const handlerRemoveTitle = (index) => {
-        const newBlogTitle = titles.filter(title => titles.indexOf(title) !== index)
+        if (e.target.type === 'checkbox') value = e.target.checked
 
-        setTitles(newBlogTitle)
+        const newFormData = {
+            ...formData,
+            [e.target.name]: value
+        }
+
+        setFormData(newFormData)
+    }
+
+    const handleTags = (e) => {
+        let { tags, ...others } = formData;
+
+        tags.includes(e.target.value) ? tags = tags.filter(tag => tag !== e.target.value) : tags = [...tags, e.target.value]
+
+        setFormData({
+            tags,
+            ...others
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setPosts([
+            ...posts,
+            { id: self.crypto.randomUUID(), ...formData }
+        ])
+    }
+
+    const handleRemovePost = (id) => {
+        setPosts(posts.filter(post => post.id !== id))
     }
 
     return (
-        <div className="container my-5">
-            <form action="#" onSubmit={handlerSubmit}>
-                <div className="input-group mb-3">
-                    <input
-                        className="form-control"
-                        placeholder="Title"
-                        value={newTitle}
-                        onChange={(e) => setNewTitle(e.target.value)}
-                        type="text"
-                    />
+        <div className="container my-5 px-4">
+            <h2 className='mb-4'>Articoli</h2>
+            <div>
+                <form className='border px-4 mb-5' onSubmit={handleSubmit}>
+                    <h3 className='my-4'>Nuovo Post</h3>
+                    <div>
+                        <label htmlFor='title' className='form-label'>
+                            <h5>Titolo</h5>
+                        </label>
+                        <input
+                            className="form-control mt-1 mb-2"
+                            placeholder="Titolo"
+                            type="text"
+                            id='title'
+                            name='title'
+                            value={formData.title}
+                            onChange={handleInputs}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='image' className='form-label'>
+                            <h5>Immagine</h5>
+                        </label>
+                        <input
+                            className="form-control mt-1 mb-2"
+                            placeholder="Immagine"
+                            type="text"
+                            id='image'
+                            name='imgUrl'
+                            value={formData.imgUrl}
+                            onChange={handleInputs}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='content' className='form-label'>
+                            <h5>Contenuto</h5>
+                        </label>
+                        <textarea
+                            className="form-control mt-1 mb-2"
+                            placeholder="Contenuto"
+                            rows='4'
+                            id='content'
+                            name='content'
+                            value={formData.content}
+                            onChange={handleInputs}
+                        />
+                    </div>
+                    <select className="form-select my-3">
+                        <option selected>Categoria</option>
+                        {categories.map((category, index) => (
+                            <option key={index} value={index}>{category}</option>
+                        ))}
+                    </select>
+                    <div className='my-3'>
+                        <label className='form-label'>Tags</label>
+                        {tags.map((tag, index) => (
+                            <div key={`tag${index}`} className="form-check">
+                                <input
+                                    className='form-check-input'
+                                    type="checkbox"
+                                    id={`tag${index}`}
+                                    value={tag}
+                                    onChange={handleTags}
+                                />
+                                <label className='form-label' htmlFor={`tag${index}`}>{tag}</label>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="form-check">
+                        <input
+                            type="checkbox"
+                            className='form-check-input'
+                            id='published'
+                            name='published'
+                            checked={formData.published}
+                            onChange={handleInputs}
+                        />
+                        <label className='form-check-label' htmlFor="published">Pubblicato</label>
+                    </div>
                     <button
-                        className="btn btn-primary"
+                        className="btn btn-primary my-4"
                         type='submit'>
                         Invia
                     </button>
-                </div>
-            </form>
+                </form>
 
-            <ul className='list-group'>
-                {titles.map((title, index) => (
-                    <li key={index} className='list-group-item d-flex justify-content-between'>
-                        <span>{title}</span>
-                        <i
-                            className="fa-regular fa-trash-can"
-                            onClick={() => handlerRemoveTitle(index)}
-                        ></i>
-                    </li>
-                ))}
-            </ul>
+                <div className='border px-3 py-3'>
+                    <h4>Lista Post</h4>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Immagine</th>
+                                <th scope="col">Titolo</th>
+                                <th scope="col">Contenuto</th>
+                                <th scope="col">Categeoria</th>
+                                <th scope="col">Tags</th>
+                                <th scope="col">Pubblicato</th>
+                                <th scope="col">Azioni</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {posts.map(post => (
+                                <tr key={post.id}>
+                                    <th scope="row">
+                                        <img src={post.imgUrl} alt={post.title} />
+                                    </th>
+                                    <td>{post.title}</td>
+                                    <td>{post.content}</td>
+                                    <td>{post.category}</td>
+                                    <td>{post.tags.join(',')}</td>
+                                    <td>{post.published ? 'Si' : 'No'}</td>
+                                    <td>
+                                        <button
+                                            className='btn btn-warning'
+                                            onClick={() => handleRemovePost(post.id)}
+                                        >
+                                            <i className="fa-solid fa-trash-can"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
